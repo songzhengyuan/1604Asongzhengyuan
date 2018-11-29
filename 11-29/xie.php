6a1790b38309634d488a403aed92c7403d4a7d14
songquan <253289672@qq.com>
2018-11-29 09:07:13 +0800

    1

---
+++ 11-29/xie.php
@@ -0,0 +1,24 @@
+<?php 
+// 数组函数
+array();创建一个数组$aaa = array();
+in_array();是不是数组$a=array('1');in_array($a);
+array_pop();数组最后一个出栈$a =array('1');array_pop($a);
+array_push();数组最后一个进栈$a =array('1');array_push($a);
+array_shift();数组第一个出栈$a =array('1');array_shift($a);
+array_unshift();数组第一个进栈$a =array('1');array_unshift($a);
+array_replace(a,b);替换$a=array('1');$b=array('2');array_replace($a,$b)
+array_sum();数组求和$a=array('1');array_sum($a);
+array_values();返回数组所有值$a=array('1');array_values($a);
+sort();排序$a=array('1');sort($a);
+rsort();逆向排序$a=array('1');rsort($a);
+// 字符串函数
+echo();输出
+MD5();加密
+print();输出字符串
+print_r();输出格式化的字符串
+var_dump();
+strlen();返回长度
+strrev();逆向输出
+strpos();第一次出现
+strrchr();最后一次出现
+strstr();字符串首次出现
